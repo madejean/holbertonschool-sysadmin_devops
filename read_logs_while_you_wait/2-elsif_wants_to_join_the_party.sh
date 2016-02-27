5f4cdced8116b691227dc890182d66d8bc6ec821
madejean <madejean007@hotmail.fr>
2016-02-27 14:58:43 -0800

    look up count

--- read_logs_while_you_wait/2-elsif_wants_to_join_the_party.sh
+++ read_logs_while_you_wait/2-elsif_wants_to_join_the_party.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
-while IFS='' read -r line;
-if 
-do
-    echo $line
-done < "$1"
+while read -r line; do
+    if $line=="HEAD"
+    then
+	echo count"HEAD"
+    elif $line== "GET"
+    then
+	echo count"GET"
 fi
+done < "$1"
+
