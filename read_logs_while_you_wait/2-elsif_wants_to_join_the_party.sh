982828ee7693aed71660879c700c79838c2d9685
madejean <madejean007@hotmail.fr>
2016-03-01 11:20:24 -0800

    removed conflict

--- read_logs_while_you_wait/2-elsif_wants_to_join_the_party.sh
+++ read_logs_while_you_wait/2-elsif_wants_to_join_the_party.sh
@@ -3,17 +3,14 @@ while read line
 do
     if [[ $line == *HEAD* ]]
     then
-<<<<<<< HEAD
 	HEAD=$(($HEAD+1))
     elif [[ $line == *GET* ]]
     then
 	GET=$(($GET+1))
-=======
-	 HEAD=$(($HEAD+1))
+	HEAD=$(($HEAD+1))
     elif [[ $line == *GET* ]]
     then
 	 GET=$(($GET+1))
->>>>>>> 0bc77bc5d6db5fdb697d399ab35dd3a7fec90c1b
     fi
 done < $1
 echo $HEAD
