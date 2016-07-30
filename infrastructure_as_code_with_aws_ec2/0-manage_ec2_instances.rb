c211f5052f8d03ed8ea761e425c6b1b91e7bf531
madejean <madejean007@hotmail.fr>
2016-07-30 00:14:48 +0000

    updated progress

--- infrastructure_as_code_with_aws_ec2/0-manage_ec2_instances.rb
+++ infrastructure_as_code_with_aws_ec2/0-manage_ec2_instances.rb
@@ -1,50 +1,40 @@
-#!/usr/bin/ruby
-
-require 'rubygems'
 require 'aws-sdk'
+require 'base64'
+export AWS_CREDENTIAL_PROFILES_FILE = ~/.ssh
 
-Aws.config(:credentials => ENV['AWS_ACCES_KEY_ID'],
-           :credentials => ENV['AWS_SECRET_KEY'])
+# User code that's executed when the instance starts
+script = ''
 
-ec2 = Aws::EC2.new.regions['us-west-2b']
-ami_name = 'amzn-ami-hvm-2016.03.3.x86_64-gp2 (ami-7172b611)'
-key_pair_name = qwikLABS-L265-609409
-private_key_file = "#{ENV['HOME']}/.ssh/AmazonSecretAccessKey.pem"
-security_group_name = 'launch-wizard-1'
-instance_type = 't2.micro'
-ssh_username = amzn-ami
+encoded_script = Base64.encode64(script)
 
+ec2 = Aws::EC2::Resource.new(region: 'us-west-2')
 
-image = Aws.memoize do
-  ec2.images.filter("root-device-type", "ebs").filter('name', ami_name).first
-end
+instance = ec2.create_instances({
+  image_id: 'IMAGE_ID',
+  min_count: 1,
+  max_count: 1,
+  key_name: 'NewInstanceKeyPair',
+  security_group_ids: ['SECURITY_GROUP_ID'],
+  user_data: encoded_script,
+  instance_type: 't2.micro',
+  placement: {
+    availability_zone: 'us-west-2'
+  },
+  subnet_id: 'SUBNET_ID',
+  network_interfaces: [{
+    device_index: 0,
+    associate_public_ip_address: true
+  }],
+  iam_instance_profile: {
+    arn: 'arn:aws:iam::' + 'ACCOUNT_ID' + ':instance-profile/aws-opsworks-ec2-role'
+  }
+})
 
-if image
-  puts "Using AMI: #{image.id}"
-else
-  raise "No image found matching #{ami_name}"
-end
+# Wait for the instance to be created, running, and passed status checks
+ec2.client.wait_until(:instance_status_ok, {instance_ids: [instance[0].id]})
 
-key_pair = ec2.key_pairs[key_pair_name]
-puts "Using keypair #{key_pair.name}, fingerprint: #{key_pair.fingerprint}"
+# Name the instance 'NewInstance' and give it the Group tag 'NewInstance'
+instance.create_tags({ tags: [{ key: 'Name', value: 'NewInstance' }, { key: 'Group', value: 'NewInstance' }]})
 
-security_group = ec2.security_groups.find{|sg| sg.name == security_group_name}
-puts "Using security group: #{security_group.name}"
-
-instance = ec2.instances.create(:image_id => image.id,
-                                :instance_type => instance_type,
-                                :count => 1,
-                                :security_groups => security_group,
-                                :key_pair => key_pair)
-puts "Launching machine ..."
-
-sleep 1 until instance.status != :pending
-puts "Launched instance #{instance.id}, status: #{instance.status}, public dns: #{instance.dns_name}, public ip: #{instance.ip_addess}"
-exit 1 unless instance.status == :running
-
-puts "Launched: You can SSH to it with;"
-puts "ssh -i #{private_key_file} #{ssh_username}@#{instance.ip_address}"
-
-
-  
-  
+puts instance.id
+puts instance.public_ip_address
