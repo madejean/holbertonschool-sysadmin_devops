c6e8c91c65c2d3c5203f775685535ac481ef804f
madejean <madejean007@hotmail.fr>
2016-08-21 21:18:02 +0000

    added parser options

--- build_your_aws_s3_script/0-basic_aws_s3_script.rb
+++ build_your_aws_s3_script/0-basic_aws_s3_script.rb
@@ -11,10 +11,22 @@ OptionParser.new do |opts|
       options[:action] = a
     end
 
-    opts.on("-h", "--help") do
-      puts opts
-      exit
-    end
+    opts.on("-v", "--verbose", "Run verbosely") do |v|
+        options[:verbose] = v
+      end
+
+      opts.on("-b", "--bucketname=BUCKET_NAME", "Name of the bucket to perform the action on") do |b|
+        options[:bucket_name] = b
+      end
+
+      opts.on("-f", "--filepath=FILE_PATH", "Path to the file to upload") do |f|
+        options[:file_path] = f
+      end
+
+      opts.on("-h", "--help") do
+          puts opts
+          exit
+      end
 
 end.parse!
 
